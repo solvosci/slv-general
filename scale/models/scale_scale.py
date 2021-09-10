48ca5d04e5db09427aec5e9321e5ae4f0fca5a3b
David Alonso // Solvos <david.alonso@solvos.es>
2021-09-10 09:38:47 +0200

    [FIX] scale: protocol adjustments

--- scale/models/scale_scale.py
+++ scale/models/scale_scale.py
@@ -48,67 +48,141 @@ class Scale(models.Model):
         socket.setdefaulttimeout(self.answer_time / 1000)
         server_address = (self.ip, self.port)
 
+        command_ask = self.command_ask.encode() + b'\r'
+
         error_socket = False
         for attempt in range(self.attempt_number):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             if error_socket:
                 time.sleep(self.time_between_attempt / 1000)
             try:
-                sock.connect(server_address)
-                error_socket = False
-                break
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    sock.settimeout(self.answer_time / 1000)
+                    t_ini = fields.datetime.now()
+                    sock.connect(server_address)
+                    sock.send(command_ask)
+                    try:
+                        data = sock.recv(16)
+                        # Connection accepted and answer received
+                        #  => we've finished with TCP connection and attempts
+                        error_socket = False
+                        ts = fields.datetime.now() - t_ini
+                    except socket.timeout as st:
+                        # Connection accepted, but not answer not received
+                        error_socket = st
+                        continue
             except socket.error as error:
+                # Connection refused
                 error_socket = error
+                continue
 
-        if error_socket:
-            return {
-                "value": "----",
-                "err": "%s after %d attempt(s)" % (
-                    error_socket, self.attempt_number
-                )
-            }
+            success_msg = "Attempt #%d completed in %d ms" % (
+                attempt + 1, int(1000*ts.total_seconds())
+            )
 
-        try:
-            sock.settimeout(self.answer_time / 1000)
-            command_ask = self.command_ask.encode()
-            sock.sendall(command_ask)
-
-            data = sock.recv(16)
             strdata = data.decode()
             if self.answer_prefix:
-                if self.answer_prefix != strdata[:len(self.answer_prefix)]:
+                if self.answer_prefix != strdata[-len(self.answer_prefix):]:
                     return {
                         "value": "----",
                         "err": _(
                             "Unexpected frame format received (%s)."
-                            " Expected: '%s12345'"
+                            " Expected: ' 12345.$0D%s'"
                         ) % (strdata, self.answer_prefix)
                     }
-                strdata = strdata[len(self.answer_prefix):]
+                strdata = strdata[:6]
 
-                try:
-                    data = int(strdata)
-                except ValueError:
-                    return {
-                        "value": "----",
-                        "err": _(
-                            "Unexpected received number format (%s)"
-                        ) % strdata
-                    }
+            try:
+                data = int(strdata)
+            except ValueError:
+                return {
+                    "value": "----",
+                    "err": _(
+                        "Unexpected received number format ('%s')"
+                    ) % strdata
+                }
 
             return {
                 "value": data,
-                "err": False
+                "err": False,
+                "ok": success_msg,
             }
 
-        except socket.timeout:
-            return {
-                "value": "----",
-                "err": _("Timed out on weighing scale")
-            }
+        # If for loop is finished and return is not performed,
+        #  we've reached total attempts
+        return {
+            "value": "----",
+            "err": "%s after %d attempt(s)" % (
+                error_socket, self.attempt_number
+            )
+        }
 
-        finally:
-            sock.close()
+        # except socket.timeout:
+        #     return {
+        #         "value": "----",
+        #         "err": _("Timed out on weighing scale")
+        #     }
+
+        # error_socket = False
+        # for attempt in range(self.attempt_number):
+        #     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #     if error_socket:
+        #         time.sleep(self.time_between_attempt / 1000)
+        #     try:
+        #         sock.connect(server_address)
+        #         error_socket = False
+        #         break
+        #     except socket.error as error:
+        #         error_socket = error
+
+        # if error_socket:
+        #     return {
+        #         "value": "----",
+        #         "err": "%s after %d attempt(s)" % (
+        #             error_socket, self.attempt_number
+        #         )
+        #     }
+
+        # try:
+        #     sock.settimeout(self.answer_time / 1000)
+        #     command_ask = self.command_ask.encode() + b'\r\n'
+        #     sock.sendall(command_ask)
+
+        #     data = sock.recv(16)
+        #     strdata = data.decode()
+        #     if self.answer_prefix:
+        #         if self.answer_prefix != strdata[-len(self.answer_prefix):]:
+        #             return {
+        #                 "value": "----",
+        #                 "err": _(
+        #                     "Unexpected frame format received (%s)."
+        #                     " Expected: ' 12345.$0D%s'"
+        #                 ) % (strdata, self.answer_prefix)
+        #             }
+        #         strdata = strdata[:6]
+
+        #         try:
+        #             data = int(strdata)
+        #         except ValueError:
+        #             return {
+        #                 "value": "----",
+        #                 "err": _(
+        #                     "Unexpected received number format ('%s')"
+        #                 ) % strdata
+        #             }
+
+        #     return {
+        #         "value": data,
+        #         "err": False
+        #     }
+
+        # except socket.timeout:
+        #     return {
+        #         "value": "----",
+        #         "err": _("Timed out on weighing scale")
+        #     }
+
+        # finally:
+        #     sock.close()
 
     def test_get_weight(self):
         self.ensure_one()
@@ -116,6 +190,6 @@ class Scale(models.Model):
         msg = (
             ret.get("err", False)
             or
-            _("Success!! Weight read: %d") % ret["value"]
+            _("Success!! %s\n\nWeight read: %d") % (ret["ok"], ret["value"])
         )
         raise UserError(msg)
