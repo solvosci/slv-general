2ebd7046929d935d13aba8020ebe155a97a6e0a2
ChristianSantamaria <christiancameselle@hotmail.com>
2021-10-21 16:21:43 +0200

    [ADD] camera

---
+++ camera/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    'name': 'Camera',
+    'author': 'Solvos',
+    'category': 'Tools',
+    'summary': 'Adds a basic webcam management',
+    'website': 'https://github.com/solvosci/slv-general',
+    'license': 'LGPL-3',
+    'version': '13.0.1.0.0',
+    'depends': [],
+    'data': [
+        'data/camera_data.xml',
+        'security/camera_security.xml',
+        'security/ir.model.access.csv',
+        'views/camera_actions.xml',
+        'views/camera_views.xml',
+    ],
+}
