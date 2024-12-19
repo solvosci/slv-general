282362d588fd262f176d4c561038f60fa6c92039
Christian Santamaria <christian.santamaria@solvos.es>
2024-12-19 10:52:25 +0100

    [ADD] ir_filter_security_extra

---
+++ ir_filter_security_extra/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    'name': 'Ir Filter Security Extra',
+    'summary': 'Adds new group that allows to remove any filter.',
+    'author': 'Solvos',
+    'category': 'Human Resources/Fleet',
+    'website': 'https://github.com/solvosci/slv-general',
+    'license': 'LGPL-3',
+    'version': '13.0.1.0.0',
+    'data': [
+        'security/ir_filter_security_extra.xml',
+    ],
+    'application': True,
+}
