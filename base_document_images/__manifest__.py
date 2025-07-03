3334bd165bb688a5d81b7390890edebe503b3941
Christian Santamaria <christian.santamaria@solvos.es>
2025-07-03 14:37:40 +0200

    [ADD] base_document_images Provides reusable image fields (e.g., signature, stamp) for business documents

---
+++ base_document_images/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Base Document Images",
+    "summary": """
+        Provides reusable image fields (e.g., signature, stamp) for business documents
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/solvosci/slv-general",
+    "depends": [],
+    "data": [
+        "views/res_company_views.xml",
+    ],
+    "installable": True,
+}
---
+++ base_document_images/readme/USAGE.rst
@@ -0,0 +1,6 @@
+1. Navigate to the desired Company:
+    - Go to **Settings** > **Companies**.
+    - Select the company you want to configure.
+2. In the **Document Images** tab:
+    - Upload the **Image**.
+3. Save your changes.
