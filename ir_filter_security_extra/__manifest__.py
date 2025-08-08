78e8d3c58dfc83c52958a70fa96be824a8f52daf
Dante Pereyra <dante.pereyra@solvos.es>
2025-08-08 13:40:02 +0200

    [MIG] ir_filter_security_extra: Migration to 17.0

--- ir_filter_security_extra/__manifest__.py
+++ ir_filter_security_extra/__manifest__.py
@@ -7,7 +7,7 @@
     'category': 'Human Resources/Fleet',
     'website': 'https://github.com/solvosci/slv-general',
     'license': 'LGPL-3',
-    'version': '13.0.1.0.0',
+    'version': '17.0.1.0.0',
     'data': [
         'security/ir_filter_security_extra.xml',
     ],
