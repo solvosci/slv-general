faa6acf57afb636f36f1b11ffe5d6a4dc04a7db7
Dante Pereyra <dante.pereyra@solvos.es>
2025-08-08 15:06:01 +0200

    [MIG] camera: Migration to 17.0

--- camera/__manifest__.py
+++ camera/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'Adds a basic webcam management',
     'website': 'https://github.com/solvosci/slv-general',
     'license': 'LGPL-3',
-    'version': '13.0.1.0.0',
+    'version': '17.0.1.0.0',
     'depends': [],
     'data': [
         'data/camera_data.xml',
