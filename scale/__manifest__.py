b9b092bda5daf340b7160245e44e065431123a9e
Christian Santamaria <christian.santamaria@solvos.es>
2025-08-11 10:52:42 +0200

    [MIG] scale: Migration to 17.0

--- scale/__manifest__.py
+++ scale/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'Adds a basic scale management',
     'website': 'https://github.com/solvosci/slv-general',
     'license': 'LGPL-3',
-    'version': '13.0.3.0.0',
+    'version': '17.0.1.0.0',
     'depends': ['uom', 'mail'],
     'data': [
         'data/scale_data.xml',
