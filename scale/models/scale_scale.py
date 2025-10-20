abe0b0b2374936eaa58f63c43cf8a263c8aa3c7f
Iria Alonso <iria.alonso@solvos.es>
2025-10-20 10:21:55 +0200

    [IMP] scale: get weight with webservice without caching

--- scale/models/scale_scale.py
+++ scale/models/scale_scale.py
@@ -165,6 +165,7 @@ class Scale(models.Model):
         self.get_time_parameters()
         timeout = self.answer_time / 1000
         error_http = None
+        headers = {"Cache-Control": "no-cache"}
 
         for attempt in range(self.attempt_number):
             if error_http and attempt > 0:
@@ -172,7 +173,7 @@ class Scale(models.Model):
 
             try:
                 t_ini = fields.Datetime.now()
-                response = requests.get(self.url, timeout=timeout)
+                response = requests.get(self.url, headers=headers, timeout=timeout)
                 response.raise_for_status()
                 ts = fields.Datetime.now() - t_ini
 
